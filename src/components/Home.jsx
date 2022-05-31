12b1f3f6b4fddff267c22593ba11810ffdd50aef
SUMANTA5 <sumanta111195@gmail.com>
2022-05-31 12:58:13 +0530

    Products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import Products from "./Products";
 
 const Home = () => {
   return (
     <div className="hero">
       <div className="card bg-dark text-white border-0">
-        <img src="/assets/bg.jpg" className="card-img" alt="Background" 
-        height='550px'/>
+        <img
+          src="/assets/bg.jpg"
+          className="card-img"
+          alt="Background"
+          height="550px"
+        />
         <div className="card-img-overlay d-flex flex-column justify-content-center">
-            <div className="container">
-            <h5 className="card-title display-3 fw-bolder mb-0">NEW ARRIVALS</h5>
-          <p className="card-text lead fs-2">
-            CHECK OUT ALL THE TRENDS
-          </p>
+          <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">
+              NEW ARRIVALS
+            </h5>
+            <p className="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+          </div>
         </div>
-            </div>
       </div>
+      <Products/>
     </div>
   );
 };
