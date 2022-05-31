36f3852ece26392afc0671074bedd678e8c811e4
SUMANTA5 <sumanta111195@gmail.com>
2022-05-31 21:59:41 +0530

    clothing

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -25,19 +26,63 @@ const Products = () => {
   }, []);
 
   const Loading = () => {
-    return <>Loading.....</>;
+    return (
+      <>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+        <div className="col-md-3">
+          <Skeleton height={350} />
+        </div>
+      </>
+    );
   };
+
+  const filterProduct = (cat) => {
+    const updatedList = data.filter((x)=> x.category === cat);
+    setFilter(updatedList);
+  }
+
   const ShowProducts = () => {
     return (
       <>
         <div className="buttons d-flex justify-content-center mb-5 pb-5">
-          <button className="btn btn-outline-dark me-2">All</button>
-          <button className="btn btn-outline-dark me-2">Men's Clothing</button>
-          <button className="btn btn-outline-dark me-2">
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
             women's Clothing
           </button>
-          <button className="btn btn-outline-dark me-2">Jewelery </button>
-          <button className="btn btn-outline-dark me-2">Electronic </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("jewelery")}
+          >
+            Jewelery{" "}
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronic{" "}
+          </button>
         </div>
         {filter.map((product) => {
           return (
@@ -77,7 +122,7 @@ const Products = () => {
           </div>
         </div>
         <div className="row d-flex justify-content-center">
-          <ShowProducts />
+          {loading ? <Loading /> : <ShowProducts />}
         </div>
       </div>
     </div>
