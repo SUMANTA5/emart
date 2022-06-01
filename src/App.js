944226779c22f6cfb47543a6e4b84b01827b48e1
SUMANTA5 <sumanta111195@gmail.com>
2022-06-01 11:11:11 +0530

    all fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import './App.css';
-import Home from './components/Home';
-import Navebar from './components/Navebar';
-import {Switch, Route} from 'react-router-dom';
-import Products from './components/Products';
-import Product from './components/Product';
-import Cart from './components/Cart';
+import "./App.css";
+import Home from "./components/Home";
+import Navebar from "./components/Navebar";
+import { Switch, Route } from "react-router-dom";
+import Products from "./components/Products";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <>
-     <Navebar/>
-    <Switch/>
-   <Route exact path="/" component={Home}/>
-   <Route exact path="/products" component={Products}/>
-   <Route exact path="/products/:id" component={Product}/>
-   <Route exact path="/cart" component={Cart} />
-    <Switch/>
+      <Navebar />
+      <Switch />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/products/:id" component={Product} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Switch />
     </>
   );
 }
