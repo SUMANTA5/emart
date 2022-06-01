213274c9f4be9ff64cd2057f5c58d726e737b876
SUMANTA5 <sumanta111195@gmail.com>
2022-06-01 10:43:09 +0530

    cart fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navebar from './components/Navebar';
 import {Switch, Route} from 'react-router-dom';
 import Products from './components/Products';
 import Product from './components/Product';
+import Cart from './components/Cart';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
    <Route exact path="/" component={Home}/>
    <Route exact path="/products" component={Products}/>
    <Route exact path="/products/:id" component={Product}/>
+   <Route exact path="/cart" component={Cart} />
     <Switch/>
     </>
   );
