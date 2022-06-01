45f459f76c8b3e30af5ebfada15ed60f50586a87
SUMANTA5 <sumanta111195@gmail.com>
2022-06-01 07:52:50 +0530

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Home from './components/Home';
 import Navebar from './components/Navebar';
+import {Switch, Route} from 'react-router-dom';
+import Products from './components/Products';
 
 function App() {
   return (
     <>
-    <Navebar/>
-    <Home/>
+     <Navebar/>
+    <Switch/>
+   <Route exact path="/" component={Home}/>
+   <Route exact path="/products" component={Products}/>
+    <Switch/>
     </>
   );
 }
